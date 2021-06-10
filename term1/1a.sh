f2aff8af629d1c5eedcd413bf58b26dec212d39b
Keshav Singhal <keshavsinghal594@gmail.com>
2021-06-10 11:19:53 +0530

    removed term1

--- term1/1a.sh
+++
@@ -1,6 +0,0 @@
-# program number: 1
-# Student Name: Keshav Singhal; Register Number: IMT2018511
-# Date: 25/02/2021
-# Description : Create the following types of a files using shell command.
-#!/bin/bash
-ln -s input.txt softlink.txt
